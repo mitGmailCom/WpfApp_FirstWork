f3ed917db163ae0588f5f1278b52fed02f4cf7d2
Admin <Admin@X>
2018-05-02 08:51:28 +0300

    Finish ColorSheme

--- WpfAppColorShemeForUser/MainWindow.xaml.cs
+++ WpfAppColorShemeForUser/MainWindow.xaml.cs
@@ -21,35 +21,56 @@ namespace WpfAppColorShemeForUser
     /// 
     public partial class MainWindow : Window
     {
+        private bool FlagUserExist { get; set; } = false;
+        private string Users { get; set; } = null;
+        private List<Theme> ListUsers;
 
-        string users = null;
-        List<Theme> ListUsers;
         public MainWindow()
         {
             InitializeComponent();
             ListUsers = new List<Theme>();
-            Theme Petya = new Theme("Petya", "Aqua", 10);
+            Theme Petya = new Theme("Petya", "Aqua", 16);
             ListUsers.Add(Petya);
             Theme Vasya = new Theme("Vasya", "GreenYellow", 12);
             ListUsers.Add(Vasya);
-            Theme Genya = new Theme("Genya", "Azure", 8);
+            Theme Genya = new Theme("Genya", "Coral", 8);
             ListUsers.Add(Genya);
             for (int i = 0; i < ListUsers.Count; i++)
             {
-                users += " " + ListUsers[i].Name;
+                Users += " " + ListUsers[i].Name;
             }
-            lblListUsers.Content = users;
+            lblListUsers.Content = Users;
         }
 
         private void btnInputUser_Click(object sender, RoutedEventArgs e)
         {
+            Theme temp = new Theme();
             if (txbUser.Text.Length == 0)
                 MessageBox.Show("Вы не авторизировались!", "Внимание");
+
             if (txbUser.Text.Length != 0)
             {
                 for (int i = 0; i < ListUsers.Count; i++)
                 {
                     if (txbUser.Text == ListUsers[i].Name)
+                    {
+                        FlagUserExist = true;
+                        temp = ListUsers[i];
+                        break;
+                    }
+
+                    if (txbUser.Text != ListUsers[i].Name)
+                        FlagUserExist = false;
+
+                }
+
+                    if (FlagUserExist == false)
+                    {
+                        MessageBox.Show("Пользователя с таким именем не существует!", "Ошибка");
+                        FlagUserExist = false;
+                    }
+
+                    if (FlagUserExist == true)
                     {
                         var txbEditor = new TextBox();
                         grid.Children.Clear();
@@ -60,38 +81,28 @@ namespace WpfAppColorShemeForUser
                         txbEditor.AcceptsTab = true;
                         txbEditor.HorizontalScrollBarVisibility = ScrollBarVisibility.Auto;
                         txbEditor.VerticalScrollBarVisibility = ScrollBarVisibility.Auto;
-                        txbEditor.Background = Brushes.Aqua;
-                        //txbEditor.Background = ListUsers[i].BorderColor;
-                        txbEditor.FontSize = ListUsers[i].FontSize;
-                        
+                        var color = (Color)ColorConverter.ConvertFromString(temp.Color);
+                        txbEditor.Background = new SolidColorBrush(color);
+                        txbEditor.FontSize = temp.FontSize;
+                        FlagUserExist = false;
                     }
-                }
             }
         }
-
-        //Theme Petya = new Theme("Aqua", 10);
-        //Theme Vasya = new Theme("GreenYellow", 10);
-        //Theme Genya = new Theme("Azure", 10);
-
-
-
-
     }
 
     public class Theme
     {
         public string Name { get; set; } = null;
-        public string BorderColor { get; set; } = null;
+        public string Color { get; set; } = null;
         public int FontSize { get; set; } = 0;
 
         public Theme()
-        {
-        }
+        { }
 
         public Theme(string name, string color, int size)
         {
             Name = name;
-            BorderColor = color;
+            Color = color;
             FontSize = size;
         }
     }
